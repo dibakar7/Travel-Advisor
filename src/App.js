0ad6214e937edbd3549ea8db247f5559f4468de8
Dibakar Biswas <88261380+dibakar7@users.noreply.github.com>
2022-08-03 08:33:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import { CssBaseline, Grid } from "@material-ui/core";
+import { getPlacesData } from "./api";
+
+import Header from './Components/Header/Header';
+import List from './Components/List/List';
+import Map from './Components/Map/Map';
+import PlaceDetails from './Components/PlaceDetails/PlaceDetails';
+
+const App = () => {
+  const [places, setPlaces] = useState([]);
+  const [childClicked, setChildClicked] = useState(null);
+
+  const [coordinates, setCoordinates] = useState();
+  const [bounds, setBounds] = useState({});
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [type, setType] = useState('restaurants');
+  const [rating, setRating] = useState(''); 
+
+  const [filteredPlaces, setFilteredPlaces] = useState([]);
+//down side code is for tracking geolocation
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
+      setCoordinates({ lat: latitude, lng: longitude });
+    })
+  })
+
+//down side code is for filter the rating
+  useEffect(()=>{
+    const filteredPlaces = places.filter((place)=> place.rating > rating)
+    setFilteredPlaces(filteredPlaces);
+  },[rating])
+
+//down side code is for showing the places inside a map within boundary and also filter places
+  useEffect(() => {
+    setIsLoading(true);
+    getPlacesData(type, bounds.sw, bounds.ne).then((data)=>{
+      setPlaces(data);
+      setFilteredPlaces([]);
+      setIsLoading(false);
+    })
+    
+  }, [type, coordinates, bounds]);
+
+  return (
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: '100%' }}>
+        <Grid item xs={12} md={4}>
+          <List
+            places={filteredPlaces.length? filteredPlaces : places}
+            childClicked={childClicked}
+            isLoading={isLoading}
+            type={type}
+            setType={setType}
+            rating={rating}
+            setRating={setRating}
+          />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Map
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+            places={filteredPlaces.length? filteredPlaces : places}
+            setChildClicked={setChildClicked}
+          />
+        </Grid>
+      </Grid>
+    </>
+  );
+}
+
+export default App;
