a69a9093a87fba4b678c1f49ea53b349389fb32d
Dibakar Biswas <88261380+dibakar7@users.noreply.github.com>
2022-10-31 09:11:33 +0530

    Update style.js

--- src/Components/Header/style.js
+++ src/Components/Header/style.js
@@ -1,21 +1,39 @@
-import { alpha, makeStyles } from "@material-ui/core/styles";
+import { alpha, makeStyles } from '@material-ui/core/styles';
+import headerBackground from './images1/header.png';
 
 export default makeStyles((theme) => ({
-    title: {
+    title1: {
         display: 'none',
         [theme.breakpoints.up('sm')]: {
             display: 'block',
         },
+        fontSize: "40px",
+
+    },
+    title2: {
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
+        },
+        fontSize: "20px",
+
+    },
+    header: {
+        backgroundImage :`url(${headerBackground})`,
+        height: '40 px',
+        width: '100%',
+
     },
     search: {
         position: 'relative',
         borderRadius: theme.shape.borderRadius,
-        backgroundColor: alpha(theme.palette.common.white, 0.15),
-        '&:hover': { backgroundColor: alpha(theme.palette.common.white, 0.25) },
+        backgroundColor: alpha(theme.palette.common.white, 0.35),
+        '&:hover': { backgroundColor: alpha(theme.palette.common.white, 0.45) },
         marginRight: theme.spacing(2),
         marginLeft: 0,
         width: '100%',
         [theme.breakpoints.up('sm')]: { marginLeft: theme.spacing(3), width: 'auto' },
+        boxShadow: "1px 3px 2px #cf8b25"
     },
     searchIcon: {
         padding: theme.spacing(0, 2), height: '100%', position: 'absolute', pointerEvents: 'none', display: 'flex', alignItems: 'center', justifyContent: 'center',
