c319de7f26f3487b6219394339d0d911a5e10b0a
Dibakar Biswas <88261380+dibakar7@users.noreply.github.com>
2022-10-31 09:16:37 +0530

    Update PlaceDetails.jsx

--- src/Components/PlaceDetails/PlaceDetails.jsx
+++ src/Components/PlaceDetails/PlaceDetails.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-//import ScrollIntoView from 'react-scroll-into-view'
 import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip } from '@material-ui/core';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import PhoneIcon from '@material-ui/icons/Phone';
@@ -10,12 +9,12 @@ import useStyles from './style';
 const PlaceDetails = ({ place, selected, refProp }) => {
     const classes = useStyles();
 
-    if(selected) refProp?.current?.scrollIntoView({ block: "start", behavior: "smooth" });
+    if(selected) refProp?.current?.scrollIntoView({ behavior: "smooth", block: "start" });
 
     return (
-        <Card elevation={6}>
+        <Card elevation={8} style={{ height: "600px"}}>
             <CardMedia
-                style={{ height: 350 }}
+                style={{ height: 250 }}
                 image={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
                 title={place.name}
             />
@@ -25,10 +24,14 @@ const PlaceDetails = ({ place, selected, refProp }) => {
                     <Rating value={Number(place.rating)} readOnly />
                     <Typography gutterBottom variant="subtitle1">out of {place.num_reviews} reviews</Typography>
                 </Box>
-                <Box display="flex" justifyContent="space-between">
-                    <Typography variant="subtitle1">Price</Typography>
-                    <Typography gutterBottom variant="subtitle1">{place.price_level}</Typography>
-                </Box>
+                <CardActions>
+                    <Button size="small" color="primary" onClick={() => window.open(place.web_url, '_blank')}>
+                        Trip Advisor
+                    </Button>
+                    <Button size="small" color="primary" onClick={() => window.open(place.website, '_blank')}>
+                        Website
+                    </Button>
+                </CardActions>
                 <Box display="flex" justifyContent="space-between">
                     <Typography variant="subtitle1">Ranking</Typography>
                     <Typography gutterBottom variant="subtitle1">{place.ranking}</Typography>
@@ -42,24 +45,16 @@ const PlaceDetails = ({ place, selected, refProp }) => {
                 {place?.cuisine?.map(({ name }) => (
                     <Chip key={name} size="small" label={name} className={classes.chip} />
                 ))}
-                {place?.address && (
+                {place.address && (
                     <Typography gutterBottom variant="body2" color="textSecondary" className={classes.subtitle}>
                         <LocationOnIcon /> {place.address}
                     </Typography>
                 )}
-                {place?.phone && (
+                {place.phone && (
                     <Typography gutterBottom variant="body2" color="textSecondary" className={classes.spacing}>
                         <PhoneIcon /> {place.phone}
                     </Typography>
                 )}
-                <CardActions>
-                    <Button size="small" color="primary" onClick={() => window.open(place.web_url, '_blank')}>
-                        Trip Advisor
-                    </Button>
-                    <Button size="small" color="primary" onClick={() => window.open(place.website, '_blank')}>
-                        Website
-                    </Button>
-                </CardActions>
             </CardContent>
         </Card>
     );
