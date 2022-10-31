f91cd7068227f6ec384ffa16e40c4538fbb82d8b
Dibakar Biswas <88261380+dibakar7@users.noreply.github.com>
2022-10-31 09:15:39 +0530

    Update style.js

--- src/Components/Map/style.js
+++ src/Components/Map/style.js
@@ -5,16 +5,17 @@ export default makeStyles(() => ({
     padding: '15px', display: 'flex', flexDirection: 'column', justifyContent: 'center', width: '100px',
   },
   mapContainer: {
-    height: '85vh', width: '139vh',
+    height: "83%", width: "207%",paddingTop: "0.4vh", position: "sticky", left: "70vh", top: "80px"
   },
   markerContainer: {
-    position: 'absolute', transform: 'translate(-50%, -50%)', zIndex: 1, '&:hover': { zIndex: 2 },paddingTop:'50px',
+    position: 'absolute', transform: 'translate(-50%, -50%)', zIndex: 1, '&:hover': { zIndex: 2 },paddingTop:'50px', borderRadius: "4px"
   },
   pointer: {
-    cursor: 'pointer',
+    cursor: "pointer",
   },
   smallCard: {
     padding: '15px', display: 'flex', flexDirection: 'column', justifyContent: 'center', width: '120px',
     cursor: 'pointer',
   }
+  
 }));
