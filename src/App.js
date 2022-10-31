497c558a9f1898aa9297bb64cfdad263e04e3145
Dibakar Biswas <88261380+dibakar7@users.noreply.github.com>
2022-10-31 09:08:49 +0530

    Update App.js
    Updated App.js, some modification were done. (31st October, 22)

--- src/App.js
+++ src/App.js
@@ -2,55 +2,79 @@ import React, { useState, useEffect } from "react";
 import { CssBaseline, Grid } from "@material-ui/core";
 import { getPlacesData } from "./api";
 
+
+import useStyles from './appStyles';
 import Header from './Components/Header/Header';
 import List from './Components/List/List';
 import Map from './Components/Map/Map';
-import PlaceDetails from './Components/PlaceDetails/PlaceDetails';
+//import PlaceDetails from './Components/PlaceDetails/PlaceDetails';
 
 const App = () => {
-  const [places, setPlaces] = useState([]);
-  const [childClicked, setChildClicked] = useState(null);
-
-  const [coordinates, setCoordinates] = useState();
-  const [bounds, setBounds] = useState({});
-
-  const [isLoading, setIsLoading] = useState(false);
+  const classes = useStyles();
   const [type, setType] = useState('restaurants');
-  const [rating, setRating] = useState(''); 
+  const [rating, setRating] = useState('');
+
+  const [coords, setCoords] = useState({});
+  const [bounds, setBounds] = useState(null);
 
   const [filteredPlaces, setFilteredPlaces] = useState([]);
+  const [places, setPlaces] = useState([]);
+
+  const [autocomplete, setAutocomplete] = useState(null);
+  const [childClicked, setChildClicked] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
 //down side code is for tracking geolocation
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
-      setCoordinates({ lat: latitude, lng: longitude });
-    })
-  })
+      setCoords({ lat: latitude, lng: longitude });
+    });
+  }, []);
 
 //down side code is for filter the rating
   useEffect(()=>{
-    const filteredPlaces = places.filter((place)=> place.rating > rating)
-    setFilteredPlaces(filteredPlaces);
-  },[rating])
+    const filtered = places.filter((place)=> Number(place.rating) > rating);
+    setFilteredPlaces(filtered);
+	  },[rating]);
+	
 
 //down side code is for showing the places inside a map within boundary and also filter places
   useEffect(() => {
-    setIsLoading(true);
-    getPlacesData(type, bounds.sw, bounds.ne).then((data)=>{
-      setPlaces(data);
+    if(bounds) {
+      setIsLoading(true);
+
+      getPlacesData(type, bounds.sw, bounds.ne).then((data)=>{
+      setPlaces(data.filter((place) => place.name && place.num_reviews > 0));
       setFilteredPlaces([]);
+      setRating('');
       setIsLoading(false);
-    })
+    });
+    }
     
-  }, [type, coordinates, bounds]);
+  }, [bounds, type]);
+
+ // console.log(places);
+  //console.log(filtered);
+
+  const onLoad = (autoC) => setAutocomplete(autoC);
+
+  //please visit google map documentation to find more information of geometry.location.lat/lng()
+  const onPlaceChanged = () => {
+      const lat = autocomplete.getPlace().geometry.location.lat();
+      const lng = autocomplete.getPlace().geometry.location.lng();
+
+      setCoords({ lat, lng });
+  };
 
   return (
     <>
+    <div className={classes.bg} style={{ width: "100%", height: "100%"}}>
       <CssBaseline />
-      <Header setCoordinates={setCoordinates}/>
+      <Header onPlaceChanged={onPlaceChanged} onLoad={onLoad}/>
       <Grid container spacing={3} style={{ width: '100%' }}>
         <Grid item xs={12} md={4}>
           <List
-            places={filteredPlaces.length? filteredPlaces : places}
+            places={filteredPlaces.length ? filteredPlaces : places}
             childClicked={childClicked}
             isLoading={isLoading}
             type={type}
@@ -61,16 +85,17 @@ const App = () => {
         </Grid>
         <Grid item xs={12} md={4}>
           <Map
-            setCoordinates={setCoordinates}
+            setCoords={setCoords}
             setBounds={setBounds}
-            coordinates={coordinates}
-            places={filteredPlaces.length? filteredPlaces : places}
+            coords={coords}
+            places={filteredPlaces.length ? filteredPlaces : places}
             setChildClicked={setChildClicked}
           />
         </Grid>
       </Grid>
+    </div>
     </>
   );
-}
+};
 
 export default App;
